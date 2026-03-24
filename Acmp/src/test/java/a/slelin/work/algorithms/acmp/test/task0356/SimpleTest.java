d71545165e70595b3c4cf640cb66fedf9a4b330a
Alexandr Slelin <a.slelin.work@mail.ru>
2026-03-24 12:53:31 +0300

    add: acmp 356 problem

---
+++ Acmp/src/test/java/a/slelin/work/algorithms/acmp/test/task0356/SimpleTest.java
@@ -0,0 +1,27 @@
+package a.slelin.work.algorithms.acmp.test.task0356;
+
+import a.slelin.work.algorithms.acmp.task0356.Main;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+
+    @Test
+    public void test1() {
+        String result = Main.solve(1000, 1100, 2, new int[][]{{1, 1}, {5, 2}});
+        assertEquals("100 250", result);
+    }
+
+    @Test
+    public void test2() {
+        String result = Main.solve(1000, 1010, 2, new int[][]{{6, 3}, {2, 2}});
+        assertEquals("10 16", result);
+    }
+
+    @Test
+    public void test3() {
+        String result = Main.solve(1000, 2000, 1, new int[][]{{10, 3}});
+        assertEquals("This is impossible.", result);
+    }
+}
